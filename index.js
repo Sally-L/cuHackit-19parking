e7d82f86cdd4dc8874cc6e64d09757f7cd708173
Sally-L <32350760+Sally-L@users.noreply.github.com>
2019-01-26 22:32:34 -0500

    Fix errors in Post.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "cuhackit",
+  user: "root",
   password: "cuhackit"
+  database: "cuhackit"
 });
 
 con.connect(function(err) {
@@ -20,9 +23,10 @@ app.use(bodyParser.json());
 app.post('/lot_info', (req,res)=>{
         console.log("in POST /lot_info");
         // Accepts JSON body { "lot_id": int, "num_cars": int}
-        let {lot_id,num_cars} = req.body;
+        var lot_id = req.body.lot_id;
+	var num_cars = req.body.num_cars;
 
-        if( !lot_id || !total_spaces_avail) {
+        if( !lot_id || !num_cars) {
                 console.log("problem with the lot_info fields");
                 return res.sendStatus(400);
         }
@@ -31,7 +35,10 @@ app.post('/lot_info', (req,res)=>{
 	var sql = "SELECT TotalSpaces FROM Lots WHERE Lot_ID = " + lot_id + ";";
 	var calculation = -num_cars;
 	con.query(sql, function (err, result) {
-		if (err) throw err;
+		if (err) {
+			throw err;
+			return res.sendStatus(404);
+		}
 		console.log("Lot " + lot_id + " Record found with " + result.TotalSpaces + "spaces.");
 		calculation += result.TotalSpaces;
 	});
@@ -42,12 +49,33 @@ app.post('/lot_info', (req,res)=>{
 	if (calculation < 0) {
 		calculation = 0;
 	}
-	var sql2 = "INSERT INTO Lot_Records (Lot_ID, Total_Spaces_Avail, Time_Stamp) VALUES (" + lot_id + ", " + calculation + ", " + Now() + ");";
+	var sql2 = "INSERT INTO Lot_Records (Lot_ID, Total_Spaces_Avail, Time_Stamp) VALUES (" + lot_id + ", " + calculation + ", Now());";
 	con.query(sql2, function (err, result) {
-		if (err) throw err;
+		if (err) {
+			throw err;
+			return res.sendStatus(400);
+		}
 		console.log("1 record inserted");
-	});	
+	});
 
 	return res.sendStatus(200);
 });
 
+/*app.get('/lot_space', (req,res)=>{
+	console.log("in GET /lot_space");
+	
+	var lot_id = req.body.lot_id;
+
+	if( !lot_id ) {
+		console.log("problem with the lot_space fields");
+		return res.sendStatus(400);
+	}
+
+	// Look up the lot_id in the database
+	var sql = "SELECT Total_Spaces_Avail
+});*/
+
+let port = 3000;
+app.listen(port, function () {
+	console.log('Listening on port '+port+'!');
+});
